1c63f39e90a1c7b834061a39c83fc2cffb3bb3a7
JoseMFT <jomaft03@gmail.com>
2022-10-09 23:13:21 +0100

    09/10/2022 - 23:12
    Movimiento y monedas listo
    Faltan las decoraciones y parte del escenario

---
+++ My project/Assets/Scripts/monedas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monedas : MonoBehaviour
+{
+    [SerializeField]
+    float velocidadRotacion = 15f;
+    [SerializeField]
+    GameObject Moneda;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (0.0f, 0.0f, velocidadRotacion * Time.deltaTime);
+    }
+    void OnTriggerEnter (Collider jugador) {
+        if (jugador.tag == "Player") {
+            Moneda.SetActive (false);
+        }
+    }
+   
+}
--- My project/Assets/Scripts/movimientoJugador.cs
+++ My project/Assets/Scripts/movimientoJugador.cs
@@ -5,49 +5,33 @@ using UnityEngine;
 public class movimientoJugador : MonoBehaviour
 {
     [SerializeField]
-    float Impulse = 15f;
+    float Impulse = 150f;
     Rigidbody sphere;
-
     Vector3 movimiento;
-    Vector3 velocidadPrevia;
+    Vector3 rotacion;
+    int contadorMonedas = 0;
     // Start is called before the first frame update
     void Start()
     {
         sphere = GetComponent<Rigidbody> ();
     }
 
-    void limits () {
-        if (movimiento.x > 15f) {
-            movimiento.x = 15f;
-        } else if (movimiento.z < -10f) {
-            movimiento.z = -10f;
-        }
-    }
-
-    void thrust () {
-            movimiento.x = velocidadPrevia.x * Impulse * (-1.75f);
-            movimiento.z = velocidadPrevia.z * Impulse * (-1.75f);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        limits ();
-        movimiento.x = Input.GetAxis ("Vertical") * Time.deltaTime * Impulse;
-        velocidadPrevia.x = movimiento.x;
-        limits ();
-        movimiento.z = Input.GetAxis ("Horizontal") * Time.deltaTime * -Impulse;
-        velocidadPrevia.z = movimiento.z;
+        movimiento.x = Input.GetAxis ("Horizontal") * Time.deltaTime * Impulse;
+        movimiento.z = Input.GetAxis ("Vertical") * Time.deltaTime * Impulse;
+    }
 
-        if (movimiento.x <= 0f) {
-            thrust ();
-        } else if (movimiento.z<= 0f) {
-            thrust ();
+    void OnTriggerEnter (Collider choque) {
+        if (choque.tag == "Tokens") {
+            ++contadorMonedas;
+            Debug.Log (contadorMonedas);
         }
-
     }
 
     private void FixedUpdate () {
         sphere.AddForce (movimiento, ForceMode.Impulse);
     }
 }
+ 
