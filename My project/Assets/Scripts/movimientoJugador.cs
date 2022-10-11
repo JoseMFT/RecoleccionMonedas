f9cb54b6af0b1cd4cfb94db845b27b3bbce6f47f
JoseMFT <jomaft03@gmail.com>
2022-10-11 13:20:56 +0100

    11/10/2022 - 13:20
    Agregada (y ocultada) función de salto

--- My project/Assets/Scripts/movimientoJugador.cs
+++ My project/Assets/Scripts/movimientoJugador.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movimientoJugador : MonoBehaviour
-{
+public class movimientoJugador: MonoBehaviour {
     [SerializeField]
     float Impulse = 150f;
     bool Play = true;
     [SerializeField]
     GameObject prefabParticles;
+    //float JumpPower = 15f;
+    //float recuperacionSalto;
 
     Rigidbody sphere;
     Vector3 movimiento;
-    Vector3 rotacion;
+    Vector3 salto;
     int contadorMonedas = 0;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
         sphere = GetComponent<Rigidbody> ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
         movimiento.x = Input.GetAxis ("Horizontal") * Time.deltaTime * Impulse;
         movimiento.z = Input.GetAxis ("Vertical") * Time.deltaTime * Impulse * 0.75f;
+        /*salto.y = JumpPower * Time.deltaTime;
+        if (Input.GetKey ("space")) {
+            sphere.AddForce (salto, ForceMode.Impulse);
+            recuperacionSalto = 5f;
+            for (recuperacionSalto = 5f; recuperacionSalto == 0f;) {
+                JumpPower = 0f;
+                recuperacionSalto = recuperacionSalto - Time.deltaTime;
+            }
+            JumpPower = 15f;
+            recuperacionSalto = 5f;
+        }*/
     }
 
     void OnTriggerEnter (Collider choque) {
@@ -32,7 +42,6 @@ public class movimientoJugador : MonoBehaviour
             ++contadorMonedas;
             Debug.Log (contadorMonedas);
             choque.GetComponent<AudioSource> ().Play ();
-            //choque.GetComponent<AudioSource> ().Play ();
             Instantiate (prefabParticles, choque.transform.position, choque.transform.rotation);
         }
     }
@@ -41,4 +50,3 @@ public class movimientoJugador : MonoBehaviour
         sphere.AddForce (movimiento, ForceMode.Impulse);
     }
 }
- 
