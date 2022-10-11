9890b869fdf92d1154424cba1187395514e1734f
JoseMFT <jomaft03@gmail.com>
2022-10-11 18:15:29 +0100

    11/10/2022 - 18:15
    Intento de rotacion de camar aXD

--- My project/Assets/Scripts/movimientoJugador.cs
+++ My project/Assets/Scripts/movimientoJugador.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class movimientoJugador: MonoBehaviour {
     [SerializeField]
     float Impulse = 150f;
+    [SerializeField]
+    float Sensitivity = 10f;
     bool Play = true;
     [SerializeField]
     GameObject prefabParticles;
@@ -13,7 +15,7 @@ public class movimientoJugador: MonoBehaviour {
 
     Rigidbody sphere;
     Vector3 movimiento;
-    Vector3 salto;
+    //Vector3 rotacion;
     int contadorMonedas = 0;
     // Start is called before the first frame update
     void Start () {
@@ -24,6 +26,14 @@ public class movimientoJugador: MonoBehaviour {
     void Update () {
         movimiento.x = Input.GetAxis ("Horizontal") * Time.deltaTime * Impulse;
         movimiento.z = Input.GetAxis ("Vertical") * Time.deltaTime * Impulse * 0.75f;
+
+        if (Input.GetKey ("left")) {
+            transform.rotation = Quaternion.Euler(0f, - Sensitivity * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey ("right")) {
+            transform.rotation = Quaternion.Euler(0f, Sensitivity * Time.deltaTime, 0f);
+        }
         /*salto.y = JumpPower * Time.deltaTime;
         if (Input.GetKey ("space")) {
             sphere.AddForce (salto, ForceMode.Impulse);
